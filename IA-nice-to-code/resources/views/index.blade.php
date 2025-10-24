ddb7c4d0f7ac8278079fe78b3ad045f338cdd191
Pedro Lima <pedrohenriquecorrealima@yahoo.com>
2025-10-24 17:57:52 -0300

    Refactor composer.json for better readability and update dev command; add new route for '/teste' returning 'index' view.

---
+++ IA-nice-to-code/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="login-interface" content="width=device-width, initial-scale=1.0">
+    <title>login-interface</title>
+</head>
+<body>
+    <h1>
+        Login
+    </h1>
+
+        <div class="user-input">
+            usuário
+            <input type="text">
+        </div>
+
+
+
+        
+    <br>
+        <div class="password-input">
+            senha
+            <input type="password">
+        </div>
+    <br>
+    <div>
+        <input type="submit" value="Entrar">
+    </div>
+</body>
+</html>
