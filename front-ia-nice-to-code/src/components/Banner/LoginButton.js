771fa35bf8da97ab18a01b69ec77dc00b22593e0
Pedro lima <116851132+PedroCorreaLima@users.noreply.github.com>
2025-10-30 09:46:37 -0300

    Add login button component

--- front-ia-nice-to-code/src/components/Banner/LoginButton.js
+++ front-ia-nice-to-code/src/components/Banner/LoginButton.js
@@ -1,3 +1,9 @@
-function LoginButton () {
-    
+function LoginButton ({ onClick, children = 'Login' }) {
+    return (
+        <button type="button" onClick={onClick} className="banner__login-button">
+            {children}
+        </button>
+    )
 }
+
+export default LoginButton
